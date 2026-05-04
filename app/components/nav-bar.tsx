15d5abd70d9c7cb6346f05eafec3b1a06d742adb
Tanay Sharda <tanaysharda@Tanays-MacBook-Air.local>
2026-05-04 12:11:01 +0530

    intro section

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,23 +1,26 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
+import headhunter from '../../public/images/logo.png';
 
 export const NavBar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 w-full">
+    <nav className="px-2 py-4 w-full z-10">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <div className="text-xl font-bold">Art of Marda</div>
-          <div className="space-x-4">
-            <a href="/" className="hover:text-blue-300">
+          <Image src={headhunter} alt="Logo" className="w-auto h-8" />
+          <div className="space-x-18">
+            <Link href="/" className="hover:text-blue-300">
               Home
-            </a>
-            <a href="/gallery" className="hover:text-blue-300">
+            </Link>
+            <Link href="/gallery" className="hover:text-blue-300">
               Gallery
-            </a>
-            <a href="/about" className="hover:text-blue-300">
+            </Link>
+            <Link href="/about" className="hover:text-blue-300">
               About
-            </a>
+            </Link>
             <ThemeToggle />
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { IntroSection } from "./components/intro-section/intro-section"
 import { NavBar } from "./components/nav-bar"
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     return (
         <div className="flex flex-col items-center min-h-screen">
             <NavBar />
+            <IntroSection />
             <h1 className="text-4xl font-bold mb-4">Welcome to Art of Marda</h1>
             <p className="text-lg text-gray-600 mb-8">Discover the art of Marda and explore our collection.</p>
             <a href="/gallery" className="px-6 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300">Explore Gallery</a>
